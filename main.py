4ef0f6b60ee75fedfc99d21c181fc728283b0d31
Dangle <le5169851@gmail.com>
2025-12-28 19:30:58 +0700

    Add: Obstacles to challenge the player

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
 import numpy as np
+from random import choice
+from typing import Sequence
 
 pygame.init()
 clock = pygame.time.Clock()
 FPS = 60
+
+class Obstacles:
+    def __init__(self, attribute: Sequence[int | float]) -> None:
+        self.attribute = np.array(attribute, dtype = np.float64)
 
 # Size
 WIDTH, HEIGHT = 800, 400
@@ -21,9 +27,17 @@ dinosour =np.array([100, LAND - DINOSOUR_HEIGHT, DINSOUR_WIDTH, DINOSOUR_HEIGHT]
 START_POINT = (0, LAND)
 END_POINT = (800, LAND)
 MAX_JUMP = 7
-# MIN_JUMP = - (MAX_JUMP + 1)
 jump_vel = MAX_JUMP
 m = 1
+
+OBSTACLE_WIDTH = [30, 40, 50, 60, 70]
+OBSTACLE_HEIGHT = 30
+# obstacle = np.array([800 - dinosour[0], LAND - OBSTACLE_HEIGHT, OBSTACLE_WIDTH, OBSTACLE_HEIGHT], dtype= np.float64)
+obstacles = []
+
+speed = 10
+obs_increment = 1000
+obs_count = 0
 
 GRAVITY = 0.2
 
@@ -32,10 +46,17 @@ is_jump = False
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def create_obs():
+    obstacle = Obstacles([1000, LAND - OBSTACLE_HEIGHT, choice(OBSTACLE_WIDTH), OBSTACLE_HEIGHT])
+    obstacles.append(obstacle)
+
+def draw_obs(obs: Obstacles):
+    pygame.draw.rect(WINDOW, RED, obs) # type: ignore
+
 # game loop
 while running:
 
-# for loop through the event queue  
+    # for loop through the event queue  
     for event in pygame.event.get():
         # Check for QUIT event      
         if event.type == pygame.QUIT:
@@ -61,13 +82,35 @@ while running:
 
             jump_vel = MAX_JUMP
             m = 1
-    print(dinosour)
+
+    obs_count += clock.tick(FPS)
+    if obs_count > obs_increment:
+        create_obs()
+        obs_increment = max(300, obs_increment - 5)
+        obs_count = 0
+        speed += 0.2
+
+    for obs in obstacles:
+        obs.attribute[0] -= speed
+
+    for obs in obstacles:
+        if obs.attribute[0] + obs.attribute[3] < 0:
+            obstacles.remove(obs)
+    # Check the current position of the object
+    # print(dinosour)
+
+    # Check the number of obstacle 
+    print(obstacles)
+
     WINDOW.fill(BLACK)
     pygame.display.set_caption("Dinosour Game")
     pygame.draw.rect(WINDOW, RED, dinosour) # type: ignore
+
+    for obs in obstacles:
+        draw_obs(obs.attribute)
+
     pygame.draw.line(WINDOW, WHITE, START_POINT, END_POINT, width= 3)
 
     pygame.display.flip()
-    clock.tick(FPS)
 
 pygame.quit()
