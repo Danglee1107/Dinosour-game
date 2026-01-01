460ff98dccebfb5c6a51b69c3dce6f7a9573084d
Dangle <le5169851@gmail.com>
2026-01-02 00:35:49 +0700

    style: improve spacing for better readability and add comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import here
 import pygame
 import numpy as np
 from random import choice, randint, getrandbits
@@ -9,11 +10,13 @@ clock = pygame.time.Clock()
 FPS = 120
 
 
-# Size
+# Window size
 WIDTH = 1000
 HEIGHT = 500
-LAND = HEIGHT // 2
 
+LAND = HEIGHT // 2
+START_POINT = (0, LAND)
+END_POINT = (WIDTH, LAND)
 
 # Color
 BLACK = (0,0,0)
@@ -24,33 +27,28 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 
+# Dino
 DINOSOUR_WIDTH = 30
 DINOSOUR_HEIGHT = 50
 dinosour = pygame.Rect(100, LAND - DINOSOUR_HEIGHT, DINOSOUR_WIDTH, DINOSOUR_HEIGHT)
 
-START_POINT = (0, LAND)
-END_POINT = (WIDTH, LAND)
-MAX_JUMP = 5
-GRAVITY = 0.2
-jump_vel = MAX_JUMP
-m = 1
 
-
+# Cactus (obstacles)
 OBSTACLE_WIDTH = [30, 40, 50, 60, 70]
 OBSTACLE_HEIGHT = [40, 50, 60]
 obstacles = []
-
-
 obs_speed= 5
 obs_increment = 1000
 obs_count = 0
 
 
+# Birds (obstacles)
 BIRD_WIDTH = 30
 BIRD_HEIGHT = 30
 h = [3,40,60]
 
 
+# Clouds (decorative objects)
 CLOUD_WIDTH = 30
 CLOUD_HEIGHT = 20
 CLOUD_SPEED = 1
@@ -58,8 +56,14 @@ MAX_CLOUD = 3
 CLOUD_SPAWN_INTERVAL = 1000 # ms
 last_spawn= 0
 cloud_h = [h for h in range(10, 125, 10)]
-cloud_rect = pygame.Rect(randint(WIDTH, WIDTH + 500), choice(cloud_h), CLOUD_WIDTH, CLOUD_HEIGHT)
 clouds: list = []
+
+
+# properties
+MAX_JUMP = 5
+GRAVITY = 0.2
+jump_vel = MAX_JUMP
+m = 1
 
 
 # Bools
@@ -69,7 +73,9 @@ is_touch = False
 down_button_held = False
 disable_jump = False
 
+
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+
 
 class Clouds:
     def __init__(self, pos: Sequence[int | float],
@@ -78,6 +84,7 @@ class Clouds:
         self.pos= np.array(pos, dtype = np.float64)
         self.size= np.array(size, dtype = np.float64)
 
+
 class Obstacles:
     def __init__(self, pos: Sequence[int | float],
                  size: Sequence[int | float], is_bird: bool= False) -> None:
@@ -85,6 +92,7 @@ class Obstacles:
         self.pos= np.array(pos, dtype = np.float64)
         self.size= np.array(size, dtype = np.float64)
         self.is_bird = is_bird
+
 
 def create_obs():
     is_bird = bool(getrandbits(1)) # 1 for bird and 0 for cactus
@@ -98,15 +106,17 @@ def create_obs():
                             size=[choice(OBSTACLE_WIDTH),rand_height])
     obstacles.append(obstacle)
 
-def create_cloud(num_cloud: int):
-    for _ in range(num_cloud):
-        cloud = Clouds(pos= [randint(WIDTH, WIDTH + 500), choice(cloud_h)],
-                    size= [CLOUD_WIDTH, CLOUD_HEIGHT])
 
-        clouds.append(cloud)
+def create_cloud():
+    cloud = Clouds(pos= [randint(WIDTH, WIDTH + 500), choice(cloud_h)],
+                size= [CLOUD_WIDTH, CLOUD_HEIGHT])
+
+    clouds.append(cloud)
+
 
 def draw_clouds(cloud: Clouds):
     pygame.draw.rect(WINDOW, BLUE, [cloud.pos, cloud.size]) #type: ignore
+
 
 def draw_obs(obs: Obstacles):
     if obs.is_bird:
@@ -156,6 +166,7 @@ while running:
             dinosour.y = LAND - dinosour.h
             disable_jump = True
 
+    # Jump physics
     keys = pygame.key.get_pressed()
     if not disable_jump:
         if keys[pygame.K_SPACE]:
@@ -176,12 +187,13 @@ while running:
                 jump_vel = MAX_JUMP
                 m = 1
 
+    # Increase difficulty when go further
     obs_count += clock.tick(FPS)
     if obs_count > obs_increment:
         create_obs()
         obs_increment = max(300, obs_increment - 5)
         obs_count = 0
-        obs_speed+= 0.2
+        obs_speed += 0.2
 
     for obs in obstacles[:]:
         obs.pos[0] -=obs_speed
@@ -200,8 +212,7 @@ while running:
     # print(obstacles)
 
     if current_time - last_spawn >= CLOUD_SPAWN_INTERVAL:
-        # num_cloud = randint(1, MAX_CLOUD)
-        create_cloud(num_cloud= 1)
+        create_cloud()
         last_spawn = current_time
 
     for cloud in clouds[:]:
@@ -213,13 +224,12 @@ while running:
     pygame.display.set_caption("Dinosour Game")
 
     for cloud in clouds[:]:
-        draw_clouds(cloud)
+        draw_clouds(cloud) # Add clouds
 
-    pygame.draw.rect(WINDOW, BLUE, cloud_rect)
     pygame.draw.rect(WINDOW, RED, dinosour) # type: ignore
 
     for obs in obstacles[:]:
-        draw_obs(obs)
+        draw_obs(obs) # Add cactus and bird
 
     pygame.draw.line(WINDOW, WHITE, START_POINT, END_POINT, width= 3)
     pygame.display.flip()
@@ -227,6 +237,5 @@ while running:
     if is_touch:
         print("GAME OVER")
         break
-
 
 pygame.quit()
