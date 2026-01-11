34ad6485040fca98a91e77c9028aa80507f7d387
Dangle <le5169851@gmail.com>
2026-01-11 18:05:25 +0700

    style: reorganize variables

--- properties.py
+++ properties.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from typing import Sequence
 
 
-@dataclass(frozen= True)
 class Config:
     FPS: int = 120
 
@@ -13,14 +12,6 @@ class Config:
     GROUND_Y: int = HEIGHT // 2
     GROUND_START: Sequence[int] = (0,GROUND_Y)
     GROUND_END: Sequence[int] = (WIDTH,GROUND_Y)
-
-@dataclass()
-class JumpConfig:
-    # properties
-    MAX_JUMP_HEIGHT: int = 5
-    GRAVITY: float = 0.2
-    jump_velocity: float = MAX_JUMP_HEIGHT
-    mass: int = 1
 
 
 @dataclass(frozen= True)
@@ -44,9 +35,9 @@ class Player:
 @dataclass(frozen= True)
 class Cactus:
     # Cactus (obstacles)
-    OBSTACLE_WIDTH: Sequence[int] = [30, 40, 50, 60, 70]
-    OBSTACLE_HEIGHT: Sequence[int] = [40, 50, 60]
-    OBSTACLE_SPEED_INCREMENT: int|float = 0.2
+    OBSTACLE_WIDTH: Sequence[int] = (30, 40, 50, 60, 70)
+    OBSTACLE_HEIGHT: Sequence[int] = (40, 50, 60)
+    OBSTACLE_SPEED_INCREMENT: float = 0.2
 
 
 @dataclass(frozen= True)
@@ -54,7 +45,7 @@ class Birds:
     # Birds (obstacles)
     BIRD_WIDTH: int = 30
     BIRD_HEIGHT: int = 30
-    BIRDS_ALTITUDES: Sequence[int]= [3,40,60]
+    BIRDS_ALTITUDES: Sequence[int]= (3,40,60)
 
 
 @dataclass(frozen= True)
@@ -64,10 +55,17 @@ class Clouds:
     CLOUD_HEIGHT: int = 20
     CLOUD_SPEED: int = 1
     CLOUD_SPAWN_INTERVAL: int = 1000 # ms
-    CLOUD_ALTITUDES: Sequence[int] = [h for h in range(10, 125, 10)]
+    CLOUD_ALTITUDES: Sequence[int] = tuple(range(10, 125, 10))
 
 
-@dataclass()
+class JumpConfig:
+    # properties
+    MAX_JUMP_HEIGHT: int = 5
+    GRAVITY: float = 0.2
+    jump_velocity: float = MAX_JUMP_HEIGHT
+    mass: int = 1
+
+
 class GameFlags:
     # Bools
     running: bool = True
